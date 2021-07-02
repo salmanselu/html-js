17de878e141b342891c99d2258208c1c68a56923
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-07-02 23:20:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
-function changeHead(){
-document.getElementById("head").innerHTML="new head";
+function toggleNightmode(){
+var body = document.getElementByTagName("body");
+var bgcolor = body.bgcolor;
+var color = body.color;
+body.bgcolor = (bgcolor=="black")?"white":"black";
+body.color = (color=="black")?"white":"black";
 }
