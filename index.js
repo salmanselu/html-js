8ef326e77997dc523e9e0aed831abc38c872b22c
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-07-02 22:58:56 +0530

    Ghhh

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 function changeHead(){
-   /* var head=document.getElementById("head");
-    head.innerHtml = "new head";*/
-document.write("function gets called");
+document.getElementById("head").innerHtml = "new head";
+//document.write("function gets called");
 }
