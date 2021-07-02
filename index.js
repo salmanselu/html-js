d666f4b9a0c7c997a80135575fa690c912e6ce2a
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-07-02 23:52:57 +0530

    Ddd

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 
 function toggleNightmode(){
-var body = document.getElementByTagName("body");
-var bgcolor = body.bgcolor;
-var color = body.color;
-body.bgcolor = (bgcolor=="black")?"white":"black";
-body.color = (color=="black")?"white":"black";
+document.getElementByTagName("body").attribute=""color:black; background-color:white;"
 }
